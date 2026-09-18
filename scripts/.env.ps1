ab21b7748ee656ca88aba49ac3a9d4544eb4a7c8
Christer Ljung <cljung@live.se>
2026-09-18 10:01:43 +0200

    upd variables

--- scripts/.env.ps1
+++ scripts/.env.ps1
@@ -1,9 +1,9 @@
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory = $true)][string]$TenantId,
+    [Parameter(Mandatory = $true)][string]$TenantId, # can be guid or foobar.onmicrosoft.com
     [Parameter(Mandatory = $true)][string]$ClientAppName = "ciam-test-app",
     [Parameter(Mandatory = $true)][string]$CustomExtensionAppName = "CIAM-custom-auth-extension-app",
-    [Parameter(Mandatory = $true)][string]$ApiHostingDomain
+    [Parameter(Mandatory = $true)][string]$ApiHostingDomain # FQDN of where you are hosting your API
 )
 
 $name = "Microsoft.Graph"
@@ -24,5 +24,13 @@ $TokenIssaunceExtName = "onTokenIssaunce"
 
 $apiEndpointPasswordMigration = "https://$apiHostingDomain/api/authenticationevent/passwordmigration"
 $apiEndpointTokenIssuance = "https://$apiHostingDomain/api/authenticationevent/tokenIssuanceStart"
+
 $certFullPath = "set full path of downloaded/JitMigrationEncryptionCert_some-number.cer"
 $keyCredentialsName = "CN=JitMigration"
+
+$Auth0TenantDomain = "...something... .auth0.com"
+$Auth0ClientId     = ""
+$Auth0ClientSecret = ""
+
+$Auth0Username     = "johndoe@live.com"
+$Auth0Password     = "..."
